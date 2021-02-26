b2ff759e789606c2b0484b296d2ff236464354bb
powerar <arpower@bell.net>
2021-02-25 19:14:10 -0500

    added user input function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert! JavaScript is running!");
+// ask the user to name their robot
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message.");
+
+console.log(10 + 10 );
+
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// call the fight function
+
+// fight();
