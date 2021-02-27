fbff0ded1ad649200dede73c98479ca570727d21
powerar <arpower@bell.net>
2021-02-27 12:36:49 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("welcome to Robot Gladiators!");
 
@@ -64,6 +64,14 @@ var fight = function() {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
+
+    // Game states
+    // "WIN" - Payer robot has defeated all enemy-robots
+    //      *Fight all enemy-robots
+    //      * Defeat each enemy-robot
+    // "LOSE" - Player robot's health is zero or less
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
