3128d0704abcb8ea72b44ad1b189d932d5898669
powerar <arpower@bell.net>
2021-03-02 17:16:28 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,22 +76,18 @@ var startGame = function() {
 
     for(var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0) {
-            // let player know what round they are in
             window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-            //pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyName = enemyNames[i];
-            // reset enemyHealth before starting new fight
-            enemyHealth = 50;
 
-            //use debugger to pause script from running and check what's going on at that moment in the code
-            debugger;
+            enemyHealth = 50;
         
-            //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            
             fight(pickedEnemyName);
         } 
         else {
         window.alert("You have lost your robot in battle! Game over!");
+        break;
         }
     }
     //after the loop extends, player is either out of health or enemies to fight, so run the endGame function
